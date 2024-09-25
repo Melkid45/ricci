95a547db437a5786d792d7c898d6196df2ddc8e4
Melkid69 <167970274+Melkid45@users.noreply.github.com>
2024-09-25 23:15:10 +0600

    Last

---
+++ app/js/main.js
@@ -0,0 +1,19 @@
+function onEntry(entry) {
+    entry.forEach(change => {
+    if (change.isIntersecting) {
+    change.target.classList.add('element-show');
+    }
+    });
+}
+
+let options = {threshold: [0.5] };
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll('.element-animation');
+
+for (let elm of elements) {
+    observer.observe(elm);
+}
+
+$('.write__body-item-bot-block-info').on('click', function(e){
+    $(this).prev('.write__body-item-bot-block-desc').toggleClass('active')
+})
