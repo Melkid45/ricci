7907412f14b06b59bcc1bd564a1c822919ca089d
Melkid69 <167970274+Melkid45@users.noreply.github.com>
2024-09-26 16:36:52 +0600

    Last

--- app/js/main.js
+++ app/js/main.js
@@ -17,3 +17,9 @@ for (let elm of elements) {
 $('.write__body-item-bot-block-info').on('click', function(e){
     $(this).prev('.write__body-item-bot-block-desc').toggleClass('active')
 })
+var splide = new Splide( '.splide', {
+    arrows : false,
+    pagination: false
+  } );
+  
+  splide.mount();
